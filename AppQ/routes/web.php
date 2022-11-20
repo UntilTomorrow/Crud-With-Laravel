fffe299e2f4edb34793866ff66bf9e6c7a3646f9
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2022-11-20 18:23:28 +0700

    Update web.php

--- AppQ/routes/web.php
+++ AppQ/routes/web.php
@@ -14,6 +14,16 @@ Route::get('/', function () {
 
 Route::get('/karyawan','\App\Http\Controllers\KaryawanController@index');
 Route::get('/karyawan/add','\App\Http\Controllers\KaryawanController@add');
+Route::get('/karyawan/edit/{id}','\App\Http\Controllers\KaryawanController@edit');
+Route::get('/karyawan/del/{id}','\App\Http\Controllers\karyawanController@del');
 Route::post('/karyawan/save','\App\Http\Controllers\KaryawanController@save');
+Route::post('/karyawan/update','\App\Http\Controllers\karyawanController@update');
+
+
+
+
+
+
+
 
 

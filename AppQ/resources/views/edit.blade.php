7ecf2aded6acc1e887d2ea55021bc4444af797d5
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2022-11-20 18:26:13 +0700

    Create edit.blade.php

---
+++ AppQ/resources/views/edit.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>App Data Karyawan</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/minty/bootstrap.min.css" integrity="sha384-H4X+4tKc7b8s4GoMrylmy2ssQYpDHoqzPa9aKXbDwPoPUA3Ra8PA5dGzijN+ePnH" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Data Karyawan</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br/>
+	
+    
+	<br/>
+	<br/>
+    @foreach($karyawan as $p)
+	<form action="/karyawan/update" method="post">
+        {{ csrf_field() }}
+        <div class="mb-3 mt-3">
+            <label for="Id" class="form-label">ID:</label>
+            <input type="hidden" value="{{ $p->id }}" class="form-control" id="Id" placeholder="Enter Id" name="id">
+        </div>
+        <div class="mb-3 mt-3">
+            <label for="Name" class="form-label">Nama</label>
+            <input type="Name" value="{{ $p->name }}" class="form-control" id="name" placeholder="Enter name" name="name">
+        </div>
+        <div class="mb-3 mt-3">
+            <label for="email" class="form-label">Email:</label>
+            <input type="email" value="{{ $p->email  }}"  class="form-control" id="email" placeholder="Enter email" name="email">
+        </div>
+        <div class="mb-3">
+            <label for="Number" class="form-label">Telepon:</label>
+            <input type="Number" value="{{ $p->telp }}" class="form-control" id="Number" placeholder="Enter Telepon" name="telp">
+        </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    @endforeach
+ 
+</body>
+</html>
